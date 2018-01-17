b68d5bac30c659fa034fc40301662d4b0ba1e791
jomino <jomino.domino@gmail.com>
2018-01-18 00:28:11 +0100

    add format to logview

--- public/logview.php
+++ public/logview.php
@@ -1,6 +1,10 @@
 <?php
 $path = __DIR__."/../app/_cache/application.log";
 if(is_file($path) && is_readable($path)){
+    print("<pre>");
     print(file_get_contents($path));
+    print("</pre>");
+}else{
+    print("<h3>no log !!</h3>");
 }
 ?>
