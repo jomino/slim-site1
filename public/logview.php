cea766ff8d51ba4fca1c1b6d92041927725e1993
jomino <jomino.domino@gmail.com>
2018-01-18 00:42:55 +0100

    idem

--- public/logview.php
+++ public/logview.php
@@ -2,7 +2,7 @@
 $path = __DIR__."/../app/_cache/application.log";
 if(is_file($path) && is_readable($path)){
     print("<pre>");
-    print(str_replace("\n",PHP_EOL,file_get_contents($path)));
+    print(str_replace('\n',PHP_EOL,file_get_contents($path)));
     print("</pre>");
 }else{
     print("<h3>no log !!</h3>");
